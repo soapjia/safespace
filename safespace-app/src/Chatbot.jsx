8596521abe8d10ab510dc61fb1b181c5300464b1
Sophia Ma <68702931+soapjia@users.noreply.github.com>
2024-09-28 16:43:04 -0400

    configured chatbot + mainscreen pages

---
+++ safespace-app/src/Chatbot.jsx
@@ -0,0 +1,10 @@
+// src/Chatbot.js
+import React from 'react';
+
+export default function Chatbot() {
+  return (
+    <div>
+      <h1>Welcome to the Chatbot!</h1>
+    </div>
+  )
+}
---
+++ safespace-app/src/Mainscreen.jsx
@@ -0,0 +1,13 @@
+// src/Mainscreen.js
+import React from 'react';
+import './Mainscreen.css';
+
+function Mainscreen() {
+  return (
+    <div className="Mainscreen">
+      <h1>welcome to your safe space!</h1>
+    </div>
+  );
+}
+
+export default Mainscreen;
